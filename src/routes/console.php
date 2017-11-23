4691f90aba61f69214e60cc9cd3e2d04368257e5
jinghewang <jinghewang@163.com>
2017-11-23 15:40:46 +0800

    自定义命令 hello

--- src/routes/console.php
+++ src/routes/console.php
@@ -16,3 +16,8 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+//自定义命令 wjh 2017-11-23
+Artisan::command('hello {project}', function ($project) {
+    $this->info("Hello {$project}!");
+})->describe('This is a example ,only output hello message.');
